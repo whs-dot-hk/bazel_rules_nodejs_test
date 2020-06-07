870b4f09adaf2c4f2c54d941997ae4fc8d16d359
whs <hswongac@gmail.com>
2020-06-07 19:02:44 +0800

    World

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import * as http from "http"
 const server = http.createServer((req, res) => {
   res.statusCode = 200
   res.setHeader("Content-Type", "text/plain")
-  res.end("Hello github")
+  res.end("Hello world")
 })
 
 server.listen(8080)
